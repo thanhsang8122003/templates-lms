dd8e8254f291b2a47585b520693d98e6604eab2a
thaibao2k3 <121879647+thaibao2k3@users.noreply.github.com>
2025-10-14 10:49:15 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./component/template/template.module').then(m => m.TemplateModule)
+  // },
   {
     path: '',
-    loadChildren: () => import('./component/template/template.module').then(m => m.TemplateModule)
-  },
-  {
-    path: 'home1',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
   {
