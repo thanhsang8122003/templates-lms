580c99fccd203641741089ebd2caba4f3f60c134
unknown <thanhsang08122003@gmail.com>
2025-10-14 13:45:14 +0700

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,26 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadChildren: () => import('./component/template/template.module').then(m => m.TemplateModule)
-  // },
   {
     path: '',
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+    loadChildren: () => import('./component/template/template.module').then(m => m.TemplateModule)
   },
-  {
-    path: 'about',
-    loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule)
-  },
-  {
-    path: 'login', 
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) 
-  },
-  {
-    path: 'roadmap',
-    loadChildren: () => import('./pages/roadmap/roadmap.module').then(m => m.RoadmapModule)
-  }
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  // },
+  // {  
+  //   path: 'about',
+  //   loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule)
+  // },
+  // {
+  //   path: 'login', 
+  //   loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) 
+  // },
+  // {
+  //   path: 'roadmap',
+  //   loadChildren: () => import('./pages/roadmap/roadmap.module').then(m => m.RoadmapModule)
+  // }
 ];
 
 @NgModule({
